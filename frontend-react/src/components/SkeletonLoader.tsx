3a9b6f4140138a227ba99a0390dd41ce5225df19
zAiro12 <lucaairoldi92@gmail.com>
2026-04-20 14:36:41 +0200

    chore: remove frontend-react directory and all related files

--- frontend-react/src/components/SkeletonLoader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SkeletonLoader({lines=3}:{lines?:number}){
-  return (
-    <div aria-hidden="true">
-      {Array.from({length:lines}).map((_,i)=> <div key={i} style={{marginBottom:8}} className="skeleton" />)}
-    </div>
-  )
-}
